98f256f5e94f549568a70e2302b49d0bfa23a850
BiancaSLougue <louguebianca@gmail.com>
2024-12-06 20:33:38 -0300

    Upload files

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const ulProjeto = document.getElementById('projects')
+
+    fetch('http://api.github.com/users/BiancaSLougue/repos')
+    .then(response => response.json())
+    .then(data => {
+
+        data.forEach(repo => {
+            let projeto = document.createElement('li')
+
+            projeto.innerHTML = `<a href='${repo.html_url}' target='_blank'>${repo.name}</a>`
+    
+            ulProjeto.appendChild(projeto)
+        });
+    })
+})
